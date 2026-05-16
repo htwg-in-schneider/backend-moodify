9a0c315fa81bac7f48b2b7670c806cc3dfba2169
sa871mil <sa871mil@htwg-konstanz.de>
2026-05-16 22:36:16 +0200

    Iteration 1b: JSON (de)serialization

--- src/main/java/de/htwg /in/schneider/moodify/backend/controller/ProductController.java
+++ src/main/java/de/htwg /in/schneider/moodify/backend/controller/ProductController.java
@@ -1,18 +1,33 @@
 package de.htwg.in.schneider.moodify.backend.controller;
 
+import de.htwg.in.schneider.moodify.backend.model.Product;
+
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
-    @GetMapping
-    public List<String> getMoods() {
-        return List.of("Happy", "Sad", "Relaxed");
+    private final List<Product> challenges = new ArrayList<>();
+
+    public ProductController() {
+        challenges.add(new Product("Drink Water", "Trinke heute 2 Liter Wasser"));
     }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return challenges;
+    }
+
+    @PostMapping
+    public Product addProducts(@RequestBody Product product) {
+    challenges.add(product);
+    return product;
+}
 }
---
+++ src/main/java/de/htwg /in/schneider/moodify/backend/model/Product.java
@@ -0,0 +1,31 @@
+package de.htwg.in.schneider.moodify.backend.model;
+
+public class Product {
+
+    private String title;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
