9bf1e55fb09c828a6b9d39b7c9d2e62b0a2d5247
sa871mil <sa871mil@htwg-konstanz.de>
2026-05-19 19:25:25 +0200

    teration 4: CRUD for Products

--- src/main/java/de/htwg /in/schneider/moodify/backend/controller/ProductController.java
+++ src/main/java/de/htwg /in/schneider/moodify/backend/controller/ProductController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -30,8 +32,73 @@ public class ProductController {
         return repository.findAll();
     }
 
+
     @PostMapping
-    public Product addProduct(@RequestBody Product product) {
-        return repository.save(product);
+    public Product createChallenge(@RequestBody Product challenge) {
+
+
+       if (challenge.getID() != null) {
+        challenge.setID(null);
+       }
+
+       Product newChallenge = repository.save(challenge);
+
+       return newChallenge;
+
     }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateChallenge(
+
+        @PathVariable Long id,
+        @RequestBody Product challengeDetails) {
+
+        Optional<Product> opt = repository.findById(id);
+
+        if (!opt.isPresent()) {
+          return ResponseEntity.notFound().build();
+        }
+
+        Product challenge = opt.get();
+
+        challenge.setTitle(challengeDetails.getTitle());
+        challenge.setCategory(challengeDetails.getCategory());
+        challenge.setDescription(challengeDetails.getDescription());
+
+        Product updatedChallenge = repository.save(challenge);
+
+        return ResponseEntity.ok(updatedChallenge);
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteChallenge(@PathVariable Long id) {
+
+       Optional<Product> opt = repository.findById(id);
+
+       if (!opt.isPresent()) {
+          return ResponseEntity.notFound().build();
+       }
+
+       repository.delete(opt.get());
+
+       return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getChallengeById(@PathVariable Long id) {
+
+      Optional<Product> opt = repository.findById(id);
+
+      if (opt.isPresent()) {
+          return ResponseEntity.ok(opt.get());
+      } else {
+          return ResponseEntity.notFound().build();
+     }
+    }
+
+
 }
--- src/main/java/de/htwg /in/schneider/moodify/backend/controller/VisionboardController.java
+++ src/main/java/de/htwg /in/schneider/moodify/backend/controller/VisionboardController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -31,8 +33,70 @@ public class VisionboardController {
     }
 
     @PostMapping
-    public Visionboard addVisionboard(@RequestBody Visionboard visionboard) {
-        return repository.save(visionboard);
+    public Visionboard createVisionboard(@RequestBody Visionboard visionboard) {
+
+
+       if (visionboard.getID() != null) {
+        visionboard.setID(null);
+       }
+
+       Visionboard newVisionboard = repository.save(visionboard);
+
+       return newVisionboard;
+
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Visionboard> updateVisionboard(
+
+        @PathVariable Long id,
+        @RequestBody Visionboard visionboardDetails) {
+
+        Optional<Visionboard> opt = repository.findById(id);
+
+        if (!opt.isPresent()) {
+          return ResponseEntity.notFound().build();
+        }
+
+        Visionboard vs = opt.get();
+
+        vs.setTitle(visionboardDetails.getTitle());
+        vs.setCategory(visionboardDetails.getCategory());
+        vs.setCreatedAt(visionboardDetails.getCreatedAt());
+        vs.setImageUrls(visionboardDetails.getImageUrls());
+
+        Visionboard updatedVisionboard = repository.save(vs);
+
+        return ResponseEntity.ok(updatedVisionboard);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteVisionboard(@PathVariable Long id) {
+
+       Optional<Visionboard> opt = repository.findById(id);
+
+       if (!opt.isPresent()) {
+          return ResponseEntity.notFound().build();
+       }
+
+       repository.delete(opt.get());
+
+       return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Visionboard> getVisionboardById(@PathVariable Long id) {
+
+      Optional<Visionboard> opt = repository.findById(id);
+
+      if (opt.isPresent()) {
+          return ResponseEntity.ok(opt.get());
+      } else {
+          return ResponseEntity.notFound().build();
+     }
+    }
+
     
 }
