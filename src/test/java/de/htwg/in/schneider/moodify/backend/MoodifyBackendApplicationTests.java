2f2ff87e45c29da9264be316115d84f3f0a6e61f
sa871mil <sa871mil@htwg-konstanz.de>
2026-05-20 12:53:22 +0200

    Test

---
+++ src/test/java/de/htwg/in/schneider/moodify/backend/MoodifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwg.in.schneider.moodify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
