402414e2809543cc1e1660cf7e17de7b29e02263
sa871mil <sa871mil@htwg-konstanz.de>
2026-06-24 23:17:20 +0200

    test2

--- src/main/java/de/htwg/in/schneider/moodify/backend/config/DataLoader.java
+++ src/main/java/de/htwg/in/schneider/moodify/backend/config/DataLoader.java
@@ -308,26 +308,14 @@ reviewRepository.saveAll(Arrays.asList(review1, review2, review3, review4));
 
         VisionboardImages img1 = new VisionboardImages();
         img1.setImageUrl("https://htwg-in-schneider.github.io/frontend-static-moodify-2/visionboard/images-1.avif");
-        img1.setXPosition(20);
-        img1.setYPosition(40);
-        img1.setWidth(200);
-        img1.setHeight(150);
         img1.setVisionboard(vs1);
 
         VisionboardImages img2 = new VisionboardImages();
         img2.setImageUrl("https://htwg-in-schneider.github.io/frontend-static-moodify-2/visionboard/images-2.avif");
-        img2.setXPosition(250);
-        img2.setYPosition(80);
-        img2.setWidth(180);
-        img2.setHeight(120);
         img2.setVisionboard(vs1);
 
         VisionboardImages img3 = new VisionboardImages();
         img3.setImageUrl("https://htwg-in-schneider.github.io/frontend-static-moodify-2/visionboard/images-3.avif");
-        img3.setXPosition(120);
-        img3.setYPosition(250);
-        img3.setWidth(220);
-        img3.setHeight(160);
         img3.setVisionboard(vs1);
 
         vs1.setImages(Arrays.asList(img1, img2, img3));
@@ -341,26 +329,14 @@ reviewRepository.saveAll(Arrays.asList(review1, review2, review3, review4));
 
         VisionboardImages img4 = new VisionboardImages();
         img4.setImageUrl("https://htwg-in-schneider.github.io/frontend-static-moodify-2/visionboard/images-4.avif");
-        img4.setXPosition(20);
-        img4.setYPosition(40);
-        img4.setWidth(200);
-        img4.setHeight(150);
         img4.setVisionboard(vs2);
 
         VisionboardImages img5 = new VisionboardImages();
         img5.setImageUrl("https://htwg-in-schneider.github.io/frontend-static-moodify-2/visionboard/images-5.avif");
-        img5.setXPosition(250);
-        img5.setYPosition(80);
-        img5.setWidth(180);
-        img5.setHeight(120);
         img5.setVisionboard(vs2);
 
         VisionboardImages img6 = new VisionboardImages();
         img6.setImageUrl("https://htwg-in-schneider.github.io/frontend-static-moodify-2/visionboard/images-6.avif");
-        img6.setXPosition(120);
-        img6.setYPosition(250);
-        img6.setWidth(220);
-        img6.setHeight(160);
         img6.setVisionboard(vs2);
 
         vs2.setImages(Arrays.asList(img4, img5, img6));
