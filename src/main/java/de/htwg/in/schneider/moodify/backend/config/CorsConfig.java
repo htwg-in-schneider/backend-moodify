510eeccc1dfe7442648cde30f3041355b1e4553b
sa871mil <sa871mil@htwg-konstanz.de>
2026-06-24 03:15:24 +0200

    Verbesserung

--- src/main/java/de/htwg/in/schneider/moodify/backend/config/CorsConfig.java
+++ src/main/java/de/htwg/in/schneider/moodify/backend/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://htwg-in-schneider.github.io"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
