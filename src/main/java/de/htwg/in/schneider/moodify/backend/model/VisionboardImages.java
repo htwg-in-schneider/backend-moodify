ceef88d49ee4ebfa64d2047aeada08ced437ccd7
sa871mil <sa871mil@htwg-konstanz.de>
2026-06-24 23:12:38 +0200

    verbessert3

--- src/main/java/de/htwg/in/schneider/moodify/backend/model/VisionboardImages.java
+++ src/main/java/de/htwg/in/schneider/moodify/backend/model/VisionboardImages.java
@@ -52,13 +52,15 @@ public void setVisionboard(Visionboard visionboard) {
 }
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        VisionboardImages visionboardIMages = (VisionboardImages) o;
-        return id != null && id.equals(visionboard.id);
-    }
+@Override
+public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    VisionboardImages vsImages = (VisionboardImages) o;
+
+    return id != null && id.equals(that.id);
+}
 
     @Override
     public int hashCode() {
